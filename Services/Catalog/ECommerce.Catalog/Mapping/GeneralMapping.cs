087b6b3cd0354d4d6d1006cec792730715e76a5f
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2025-05-20 00:36:10 +0300

    app json fixed

--- Services/Catalog/ECommerce.Catalog/Mapping/GeneralMapping.cs
+++ Services/Catalog/ECommerce.Catalog/Mapping/GeneralMapping.cs
@@ -1,6 +1,31 @@
-﻿namespace ECommerce.Catalog.Mapping
+﻿using AutoMapper;
+using ECommerce.Catalog.Dtos.CategoryDtos;
+using ECommerce.Catalog.Dtos.ProductDetailDtos;
+using ECommerce.Catalog.Dtos.ProductDtos;
+using ECommerce.Catalog.Dtos.ProductImageDtos;
+using ECommerce.Catalog.Entities;
+
+namespace ECommerce.Catalog.Mapping
 {
-	public class GeneralMapping
+	public class GeneralMapping : Profile
 	{
+		public GeneralMapping()
+		{
+			CreateMap<Product, ResultProductDto>().ReverseMap();
+			CreateMap<Product, CreateProductDto>().ReverseMap();
+			CreateMap<Product, UpdateProductDto>().ReverseMap();
+
+			CreateMap<ProductDetail, ResultProductDetailDto>().ReverseMap();
+			CreateMap<ProductDetail, CreateProductDetailDto>().ReverseMap();
+			CreateMap<ProductDetail, UpdateProductDetailDto>().ReverseMap();
+
+			CreateMap<ProductImage, ResultProductImageDto>().ReverseMap();
+			CreateMap<ProductImage, CreateProductImageDto>().ReverseMap();
+			CreateMap<ProductImage, UpdateProductImageDto>().ReverseMap();
+
+			CreateMap<Category, ResultCategoryDto>().ReverseMap();
+			CreateMap<Category, CreateCategoryDto>().ReverseMap();
+			CreateMap<Category, UpdateCategoryDto>().ReverseMap();
+		}
 	}
 }
