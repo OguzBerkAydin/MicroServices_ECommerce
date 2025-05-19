d5e1bc50c315ebc730671b3c94063b118b2fed84
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2025-05-20 00:06:42 +0300

    Dtos added

---
+++ Services/Catalog/ECommerce.Catalog/Dtos/ProductDetailDtos/CreateProductDetailDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.Catalog.Dtos.ProductDetailDtos
+{
+	public class CreateProductDetailDto
+	{
+		public string ProductId { get; set; }
+		public string Info { get; set; }
+		public string Description { get; set; }
+	}
+
+}
---
+++ Services/Catalog/ECommerce.Catalog/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ECommerce.Catalog.Entities
+{
+	public class Product
+	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public string ImageUri { get; set; }
+		public string Description { get; set; }
+		public decimal Price { get; set; }
+		public string CategoryId { get; set; }
+		[BsonIgnore]
+		public Category Category { get; set; }
+	}
+
+}
