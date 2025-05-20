1a1bd1741543b159c24e903d7fb7ee85c62cb9fb
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2025-05-20 17:01:00 +0300

    CategoryService abstract and concrete added

---
+++ Services/Catalog/ECommerce.Catalog/Services/CategoryServices/CategoryService.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using ECommerce.Catalog.Dtos.CategoryDtos;
+using ECommerce.Catalog.Entities;
+using ECommerce.Catalog.Settings;
+using MongoDB.Driver;
+
+namespace ECommerce.Catalog.Services.CategoryServices
+{
+	public class CategoryService : ICategoryService
+	{
+		private readonly IMongoCollection<Category> _categoryCollection;
+		private readonly IMapper _mapper;
+		public CategoryService(IDatabaseSettings settings, IMapper mapper)
+		{
+			var mongoClient = new MongoClient(settings.ConnectionString);
+			var mongoDatabase = mongoClient.GetDatabase(settings.DatabaseName);
+			_categoryCollection = mongoDatabase.GetCollection<Category>(settings.CategoryCollectionName);
+			_mapper = mapper;
+		}
+		public Task<List<ResultCategoryDto>> GetAllAsync()
+		{
+			throw new NotImplementedException();
+		}
+		public Task<ResultCategoryDto> GetByIdAsync(string id)
+		{
+			throw new NotImplementedException();
+		}
+		public Task CreateAsync(CreateCategoryDto entity)
+		{
+			throw new NotImplementedException();
+		}
+		public Task UpdateAsync(UpdateCategoryDto entity)
+		{
+			throw new NotImplementedException();
+		}
+		public Task DeleteAsync(string id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Services/Catalog/ECommerce.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Catalog.Dtos.CategoryDtos;
+using ECommerce.Catalog.Entities;
+
+namespace ECommerce.Catalog.Services.CategoryServices
+{
+	public interface ICategoryService
+	{
+		Task<List<ResultCategoryDto>> GetAllAsync();
+		Task<ResultCategoryDto> GetByIdAsync(string id);
+		Task CreateAsync(CreateCategoryDto entity);
+		Task UpdateAsync(UpdateCategoryDto entity);
+		Task DeleteAsync(string id);
+	}
+}
