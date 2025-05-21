3110dea3daaf43604f929e6f52080ef0fddabbe6
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2025-05-21 17:52:27 +0300

    domain entities filled

--- Services/Order/Core/ECommerce.Order.Domain/Entities/OrderDetail.cs
+++ Services/Order/Core/ECommerce.Order.Domain/Entities/OrderDetail.cs
@@ -8,5 +8,13 @@ namespace ECommerce.Order.Domain.Entities
 {
 	public class OrderDetail
 	{
+		public int Id { get; set; }
+		public int OrderingId { get; set; }
+		public string ProductId { get; set; }
+		public string ProductName { get; set; }
+		public decimal ProductPrice { get; set; }
+		public decimal ProductTotalPrice { get; set; }
+		public int ProductAmount { get; set; }
+		public Ordering Ordering { get; set; }
 	}
 }
