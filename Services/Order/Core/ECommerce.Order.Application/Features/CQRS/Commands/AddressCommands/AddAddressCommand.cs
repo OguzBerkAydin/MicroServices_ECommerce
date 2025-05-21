e6872135189c0448d378dd92c4995ea91e117e07
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2025-05-21 18:17:40 +0300

    CQRS pattern added

---
+++ Services/Order/Core/ECommerce.Order.Application/Features/CQRS/Commands/AddressCommands/AddAddressCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Order.Application.Features.CQRS.Commands.AddressCommands
+{
+	public class AddAddressCommand
+	{
+		public string UserId { get; set; }
+		public string District { get; set; }
+		public string City { get; set; }
+		public string Detail { get; set; }
+	}
+	public class UpdateAddressCommand
+	{
+		public int Id { get; set; }
+		public string UserId { get; set; }
+		public string District { get; set; }
+		public string City { get; set; }
+		public string Detail { get; set; }
+	}
+	public class DeleteAddressCommand
+	{
+		public int Id { get; set; }
+
+		public DeleteAddressCommand(int id)
+		{
+			Id = id;
+		}
+	}
+}
---
+++ Services/Order/Core/ECommerce.Order.Application/Features/CQRS/Handlers/AddressHandlers/AddAddressCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using ECommerce.Order.Application.Features.CQRS.Commands.AddressCommands;
+using ECommerce.Order.Application.Interfaces;
+using ECommerce.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+	public class AddAddressCommandHandler
+	{
+		private readonly IRepository<Address> _repository;
+
+		public AddAddressCommandHandler(IRepository<Address> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task Handle(AddAddressCommand addAddressCommand)
+		{
+
+			var address = new Address
+			{
+				UserId = addAddressCommand.UserId,
+				District = addAddressCommand.District,
+				City = addAddressCommand.City,
+				Detail = addAddressCommand.Detail
+			};
+
+			await _repository.AddAsync(address);
+		}
+
+	}
+}
