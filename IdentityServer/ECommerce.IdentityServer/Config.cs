603602c7f8bc9baf65dfc33acb8c8d5c26e637b9
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2025-05-27 14:10:23 +0300

    Token creation precess completed successfuly

--- IdentityServer/ECommerce.IdentityServer/Config.cs
+++ IdentityServer/ECommerce.IdentityServer/Config.cs
@@ -1,8 +1,77 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace ECommerce.IdentityServer;
 
 public static class Config
 {
-    
+	public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+	{
+		new ApiResource("ResourceCatalog")
+		{
+			Scopes = { "CatalogFullPermission" }
+		},
+		new ApiResource("ResourceDiscount")
+		{
+			Scopes = { "DiscountFullPermission" }
+		},
+		new ApiResource("ResourceOrder")
+		{
+			Scopes = { "OrderFullPermission" }
+		}
+	};
+
+	public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+	{
+		new IdentityResources.OpenId(),
+		new IdentityResources.Profile(),
+		new IdentityResources.Email(),
+	};
+
+	public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+	{
+		new ApiScope("CatalogFullPermission","Full authority for catalog operations"),
+		new ApiScope("DiscountFullPermission","Full authority for Discount operations"),
+		new ApiScope("OrderFullPermission","Full authority for Order operations")
+	};
+
+	public static IEnumerable<Client> Clients => new Client[]
+	{
+		new Client
+		{
+			ClientId = "ECommerceManagerId",
+			ClientName = "ECommerce Manager User",
+			AllowedGrantTypes = GrantTypes.ClientCredentials,
+			ClientSecrets = {new Secret("ecommercesecret".Sha256())},
+			AllowedScopes = {"CatalogFullPermission"}
+		},
+		//Manager
+		new Client
+		{
+			ClientId = "ECommerceVisitorId",
+			ClientName = "ECommerce Visitor User",
+			AllowedGrantTypes = GrantTypes.ClientCredentials,
+			ClientSecrets = {new Secret("ecommercesecret".Sha256())},
+			AllowedScopes = {"CatalogFullPermission"}
+		},
+		//Admin
+		new Client
+		{
+			ClientId = "ECommerceAdminId",
+			ClientName = "ECommerce Admin User",
+			AllowedGrantTypes = GrantTypes.ClientCredentials,
+			ClientSecrets = {new Secret("ecommercesecret".Sha256())},
+			AllowedScopes = {
+				"CatalogFullPermission",
+				"DiscountFullPermission",
+				"OrderFullPermission",
+				IdentityServerConstants.LocalApi.ScopeName,
+				IdentityServerConstants.StandardScopes.Email,
+				IdentityServerConstants.StandardScopes.OpenId,
+				IdentityServerConstants.StandardScopes.Profile,
+			},
+			AccessTokenLifetime = 600
+
+		}
+	};
 }
