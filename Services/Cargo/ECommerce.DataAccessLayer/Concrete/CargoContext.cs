984c71a5369998058bf023a8e792631f0bce56c1
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2025-05-28 23:18:01 +0300

    Add Entity Framework context and project references

---
+++ Services/Cargo/ECommerce.DataAccessLayer/Concrete/CargoContext.cs
@@ -0,0 +1,23 @@
+﻿using ECommerce.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.DataAccessLayer.Concrete
+{
+	public class CargoContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=localhost,1441;Database=ECommerceCargoDb;User Id=SA;Password=SqlOguzberk3661.;");
+		}
+		public DbSet<CargoDetail> CargoDetails { get; set; }
+		public DbSet<CargoOperations> CargoOperations { get; set; }
+		public DbSet<CargoCustomer> CargoCustomers { get; set; }
+		public DbSet<CargoCompany> CargoCompanies { get; set; }
+
+	}
+}
