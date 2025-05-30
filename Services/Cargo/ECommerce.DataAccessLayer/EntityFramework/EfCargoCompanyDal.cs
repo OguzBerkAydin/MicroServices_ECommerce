fab9a72b70c107dc199783872aa706076547b520
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2025-05-30 14:29:29 +0300

    added ef

---
+++ Services/Cargo/ECommerce.DataAccessLayer/EntityFramework/EfCargoCompanyDal.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.DataAccessLayer.Abstract;
+using ECommerce.DataAccessLayer.Concrete;
+using ECommerce.DataAccessLayer.Repositories;
+using ECommerce.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.DataAccessLayer.EntityFramework
+{
+	public class EfCargoCompanyDal : GenericRepository<CargoCompany>, ICargoCompanyDal
+	{
+		public EfCargoCompanyDal(CargoContext context) : base(context)
+		{
+		}
+	}
+
+}
