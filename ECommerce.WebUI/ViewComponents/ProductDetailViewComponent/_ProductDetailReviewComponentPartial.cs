a4358d5bd544f3e05e1f4c6b3b0d5ba1067d6478
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2025-06-04 18:53:09 +0300

    #

---
+++ ECommerce.WebUI/ViewComponents/ProductDetailViewComponent/_ProductDetailReviewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.ViewComponents.ProductDetailViewComponent
+{
+	public class _ProductDetailReviewComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
