f593f7244a9bb3f7086d827cdb9fc5d0eb2bdd48
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2025-06-07 02:35:36 +0300

    #

--- ECommerce.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ ECommerce.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -2,10 +2,15 @@
 
 namespace ECommerce.WebUI.Areas.Admin.Controllers
 {
+	[Area("Admin")]
 	public class CategoryController : Controller
 	{
 		public IActionResult Index()
 		{
+			ViewBag.v0 = "Kategoriler";
+			ViewBag.v1 = "Kategoriler Listesi";
+			ViewBag.v2 = "Kategoriler Listesi";
+			ViewBag.v3 = "Kategoriler Listesi";
 			return View();
 		}
 	}
