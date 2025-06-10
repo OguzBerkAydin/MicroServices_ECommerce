7e8c26ed2e18085aee0099b0b98cbde1c0b636dc
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2025-06-11 02:40:35 +0300

    #

--- ECommerce.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
+++ ECommerce.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerce.Frontend.DtoLayer.CatalogDtos.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ECommerce.WebUI.ViewComponents.UILayoutViewComponents
 {
 	public class _FooterUILayoutComponentPartial : ViewComponent
 	{
-		public IViewComponentResult Invoke()
+		private readonly IHttpClientFactory _httpClientFactory;
+		private readonly string _apiBaseUrl = "https://localhost:44302/api/Abouts";
+
+		public _FooterUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
 		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync(_apiBaseUrl);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+
+				return View(values);
+			}
+
 			return View();
 		}
 	}
