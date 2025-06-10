9726c33152194d94774ff7091549bab28a464c2e
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2025-06-11 02:05:14 +0300

    #

---
+++ ECommerce.WebUI/Areas/Admin/Controllers/AboutController.cs
@@ -0,0 +1,109 @@
+﻿using ECommerce.Frontend.DtoLayer.CatalogDtos.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace ECommerce.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AboutController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AboutController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.v0 = "About Us";
+            ViewBag.v1 = "About Us List";
+            ViewBag.v2 = "About Us List";
+            ViewBag.v3 = "About Us List";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44302/api/Abouts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.v0 = "About Add";
+            ViewBag.v1 = "About Us";
+            ViewBag.v2 = "Add new About";
+            ViewBag.v3 = "$";
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CreateAboutDto createAboutDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createAboutDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44302/api/Abouts", content);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "About", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:44302/api/Abouts/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "About", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(string id)
+        {
+            ViewBag.v0 = "About Update";
+            ViewBag.v1 = "About Us";
+            ViewBag.v2 = "Update About";
+            ViewBag.v3 = "$";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44302/api/Abouts/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var about = JsonConvert.DeserializeObject<UpdateAboutDto>(jsonData);
+                return View(about);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(UpdateAboutDto updateAboutDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateAboutDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:44302/api/Abouts", content);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "About", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
---
+++ Frontend/ECommerce.Frontend.DtoLayer/CatalogDtos/AboutDtos/AboutDtos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Frontend.DtoLayer.CatalogDtos.AboutDtos
+{
+	public class ResultAboutDto
+	{
+		public string Id { get; set; }
+		public string Description { get; set; }
+		public string Address { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+	}
+
+	public class CreateAboutDto
+	{
+		public string Description { get; set; }
+		public string Address { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+	}
+
+	public class UpdateAboutDto
+	{
+		public string Id { get; set; }
+		public string Description { get; set; }
+		public string Address { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+	}
+}
