287f03aa20c2b08c1ca4b0974b40175ec02d064b
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2025-06-15 00:27:36 +0300

    #

---
+++ Frontend/ECommerce.Frontend.DtoLayer/CatalogDtos/ProductImageDtos/ProductImageDtos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Frontend.DtoLayer.CatalogDtos.ProductImageDtos
+{
+	public class CreateProductImageDto
+	{
+		public string Image1 { get; set; }
+		public string Image2 { get; set; }
+		public string Image3 { get; set; }
+		public string ProductId { get; set; }
+	}
+
+	public class ResultProductImageDto
+	{
+		public string Id { get; set; }
+		public string Image1 { get; set; }
+		public string Image2 { get; set; }
+		public string Image3 { get; set; }
+		public string ProductId { get; set; }
+	}
+
+	public class UpdateProductImageDto
+	{
+		public string Id { get; set; }
+		public string Image1 { get; set; }
+		public string Image2 { get; set; }
+		public string Image3 { get; set; }
+		public string ProductId { get; set; }
+	}
+}
