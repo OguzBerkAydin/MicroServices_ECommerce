8b0f08442630e927d29e70e93f79e7e3167ebd52
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2025-06-17 03:27:22 +0300

    #

---
+++ ECommerce.WebUI/Areas/Admin/Controllers/ProductDetailController.cs
@@ -0,0 +1,50 @@
+﻿using ECommerce.Frontend.DtoLayer.CatalogDtos.ProductDetailDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace ECommerce.WebUI.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class ProductDetailController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public ProductDetailController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+		[HttpGet]
+		public async Task<IActionResult> ProductsDetail(string id)
+		{
+			ViewBag.v0 = "Product detail Index";
+			ViewBag.v1 = "Product detail";
+			ViewBag.v2 = "Index Product detail";
+			ViewBag.v3 = "$";
+
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync($"https://localhost:44302/api/ProductDetails/ProductId/{id}");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var value = JsonConvert.DeserializeObject<UpdateProductDetailDto>(jsonData);
+				return View(value);
+			}
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> ProductsDetail(UpdateProductDetailDto updateProductDetailDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(updateProductDetailDto);
+			StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PutAsync("https://localhost:44302/api/ProductDetails", content);
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index", "Product", new { area = "Admin" });
+			}
+			return View();
+		}
+	}
+}
---
+++ Frontend/ECommerce.Frontend.DtoLayer/CatalogDtos/ProductDetailDtos/ProductDetailDtos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Frontend.DtoLayer.CatalogDtos.ProductDetailDtos
+{
+	public class CreateProductDetailDto
+	{
+		public string ProductId { get; set; }
+		public string Info { get; set; }
+		public string Description { get; set; }
+	}
+	public class ResultProductDetailDto
+	{
+		public string Id { get; set; }
+		public string Info { get; set; }
+		public string Description { get; set; }
+		public string ProductId { get; set; }
+	}
+	public class UpdateProductDetailDto
+	{
+		public string Id { get; set; }
+		public string Info { get; set; }
+		public string Description { get; set; }
+		public string ProductId { get; set; }
+	}
+}
