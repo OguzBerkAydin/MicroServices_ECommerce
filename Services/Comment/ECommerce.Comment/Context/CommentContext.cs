68db98da7cf293a4b49076757281902da8b7dac0
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2025-06-17 15:01:00 +0300

    Add Entity Framework Core setup for user comments
    Introduce `CommentContext` and `UserComment` entity.
    Update project file with necessary EF Core packages.
    Add migration to create `UserComments` table and
    include model snapshot for state management.

---
+++ Services/Comment/ECommerce.Comment/Context/CommentContext.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Comment.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Comment.Context
+{
+	public class CommentContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=localhost,1442;Database=ECommerceCommentDb;User Id=SA;Password=SqlOguzberk3661.;MultipleActiveResultSets=true;Encrypt=False;TrustServerCertificate=True;");
+		}
+		public DbSet<UserComment> UserComments { get; set; }
+	}
+}
